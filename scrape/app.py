8729b2cf9f160b304929768cc8cbd6b1f6890498
Brendan <eggers.brendan@gmail.com>
2020-06-19 16:56:15 -0400

    Running through game data so far so good

--- scrape/app.py
+++ scrape/app.py
@@ -2,13 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 from games import scan_cats
 
-res = requests.get('https://drinkinggamezone.com/')
-try:
-    res.raise_for_status()
-    soup_home = BeautifulSoup(res.text, 'html.parser')
-    cat_links = soup_home.select('.home-category-link')
-    for link in cat_links:
-        scan_cats(link.attrs['href'])
-except Exception as exc:
-    print('there was a problem?')
-    # Slack bot or text or something notification to me
+
+def main():
+    res = requests.get('https://drinkinggamezone.com/')
+    try:
+        res.raise_for_status()
+        soup_home = BeautifulSoup(res.text, 'html.parser')
+        cat_links = soup_home.select('.home-category-link')
+        for link in cat_links:
+            scan_cats(link.attrs['href'])
+    except Exception as exc:
+        print('there was a problem?')
+        # Slack bot or text or something notification to me
+    return
+
+
+main()
--- scrape/games.py
+++ scrape/games.py
@@ -1,6 +1,17 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 domain = "https://drinkinggamezone.com"
+
+
+def handle_individual_game_creation(game_info):
+    stats = game_info.select('.game-stats')
+    for st in stats:
+        stat_name = st.select("strong")[0].contents[0]
+        stat_val = st.contents[1]
+        if stat_name == "Type" or stat_name == "Players":
+            stat_val = re.sub(': ', '', stat_val)
+            print(stat_name, stat_val)
 
 
 def scrape_it_baby(href):
@@ -8,7 +19,7 @@ def scrape_it_baby(href):
     try:
         res.raise_for_status()
         game_info = BeautifulSoup(res.text, 'html.parser')
-        print(type(game_info))
+        handle_individual_game_creation(game_info)
     except Exception as exc:
         print('cant scrape game')
 
