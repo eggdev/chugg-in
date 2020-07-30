bfaa2c1bd3318131610b98b20ec445c642ec690a
Brendan <eggers.brendan@gmail.com>
2020-07-30 08:28:01 -0400

    Got env variables in there

--- app/App.js
+++ app/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/routes/RootNavigator';
+import env from './env';
 
 const App = () => {
+  const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = env;
   return (
     <NavigationContainer>
       <RootNavigator />
